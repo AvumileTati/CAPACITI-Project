35a38072301653198b7a1d725f9da5da16293e00
Asemahlebonani <malhilhibonani02@gmail.com>
2026-09-01 10:18:05 +0200

    chore: cleanup legacy migration scripts
    Remove deprecated Python utility scripts used for UI/CSS refactoring and add GEMINI_API_KEY to environment template.

--- rewrite_workspace.py
+++
@@ -1,8 +0,0 @@
-import sys
-import re
-
-file_path = 'src/components/TechnicianWorkspace.tsx'
-with open(file_path, 'r') as f:
-    content = f.read()
-
-# I will write a complete replacement string for the file.
